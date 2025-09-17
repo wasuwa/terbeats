47e316fdcc0928c8df5f1547b4cd3cf97762bc20
wasu <wasuwa911@gmail.com>
2025-09-18 08:56:32 +0900

    main.goをcmd/game/main.goに移動する

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -12,11 +12,14 @@ func main() {
 	app := tview.NewApplication()
 
 	// ノートの初期化
-	box := newBox()
-	note1 := newNote(box)
-	note2 := newNote(box)
-	note3 := newNote(box)
-	note4 := newNote(box)
+	box1 := newBox()
+	box2 := newBox()
+	box3 := newBox()
+	box4 := newBox()
+	note1 := newNote(box1)
+	note2 := newNote(box2)
+	note3 := newNote(box3)
+	note4 := newNote(box4)
 
 	// フレームの初期化
 	frame1 := newFrame(note1)
@@ -32,9 +35,8 @@ func main() {
 		AddItem(frame3, 0, 1, false).
 		AddItem(frame4, 0, 1, false)
 
-
-	// スペースを設定
-	wrapper := tview.NewFrame(notes).SetBorders(1, 1, 0, 0, 35, 35)
+	// スペースを設定（左右の余白を縮小して各レーンの幅を確保）
+	wrapper := tview.NewFrame(notes).SetBorders(1, 1, 0, 0, 2, 2)
 
 	// ボックスの初期位置
 	position := 0
@@ -57,10 +59,10 @@ func main() {
 				note3.AddItem(tview.NewBox(), position, 1, false)
 				note4.AddItem(tview.NewBox(), position, 1, false)
 
-				note1.AddItem(box, 1, 1, false)
-				note2.AddItem(box, 1, 1, false)
-				note3.AddItem(box, 1, 1, false)
-				note4.AddItem(box, 1, 1, false)
+				note1.AddItem(box1, 1, 1, false)
+				note2.AddItem(box2, 1, 1, false)
+				note3.AddItem(box3, 1, 1, false)
+				note4.AddItem(box4, 1, 1, false)
 			})
 
 			// 画面の一番下である場合は停止
@@ -83,19 +85,21 @@ func newBox() *tview.Box {
 	return tview.NewBox().SetBackgroundColor(tcell.ColorWhite)
 }
 
-func newNote(box *tview.Box) (*tview.Flex) {
+func newNote(box *tview.Box) *tview.Flex {
 	note := tview.NewFlex().
-						SetDirection(tview.FlexRow).
-						AddItem(box, 1, 1, false)
+		SetDirection(tview.FlexRow).
+		AddItem(box, 1, 1, false)
 	return note
 }
 
 func newFrame(note *tview.Flex) *tview.Frame {
-	return tview.NewFrame(note).SetBorders(0, 0, 0, 0, 2, 2)
+	// 各レーンの内側余白も控えめに
+	return tview.NewFrame(note).SetBorders(0, 0, 0, 0, 1, 1)
 }
 
 func isPositionBottom(root *tview.Frame, position int) bool {
 	// ターミナルの高さを取得
 	_, _, _, screenHeight := root.GetRect()
-	return position > screenHeight - 3
+	return position > screenHeight-3
 }
+
